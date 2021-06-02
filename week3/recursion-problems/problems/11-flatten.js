357d1a3387c2973dfe0c4d5018822c6e36f83d2f
Konstantin Samarin <konstsamari@gmail.com>
2021-06-02 23:10:20 +0700

    week3: recursion project

---
+++ week3/recursion-problems/problems/11-flatten.js
@@ -0,0 +1,34 @@
+/***********************************************************************
+Write a recursive function called `flatten` that takes a single array with
+any number of nested arrays and returns and array with all the nested
+contents on one level.
+
+Examples:
+
+flatten([]); // []
+flatten([1, 2]); // [1, 2]
+flatten([1, [2, [3]]]); // [1, 2, 3]
+***********************************************************************/
+
+// your code here
+
+const flatten = (arr) => {
+  let flat = [];
+
+  for (let item of arr) {
+    if (Array.isArray(item)) {
+      flat.push(...flatten(item));
+    } else {
+      flat.push(item);
+    }
+  }
+
+  return flat;
+};
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
+try {
+  module.exports = flatten;
+} catch (e) {
+  module.exports = null;
+}
